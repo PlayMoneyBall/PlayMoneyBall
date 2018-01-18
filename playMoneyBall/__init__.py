ffafc67b667731c2bf543729c3c88de1b4e9e5bd
salauddin <snalband@thoughtworks.com>
2018-01-18 22:02:14 +0530

    Added basic matrix to display player details

--- playMoneyBall/__init__.py
+++ playMoneyBall/__init__.py
@@ -1,9 +1,18 @@
 def reader():
     with open("./data/211028.csv", 'r') as file:
         line = file.read()
-        print(line ,"---> ")
-        print(line.split(",")," <---")
-
+        data = line.split("\n")
+        players={}
+        for i, value in enumerate(data):
+            value_length = len(value.split(','))
+            if value_length == 11:
+                over_wise_data = value.split(',');
+                batsman=over_wise_data[4];
+                if batsman in players:
+                    players[batsman]= players.get(batsman) + int(over_wise_data[7])
+                else:
+                    players[batsman]= int(over_wise_data[7];
+        print(players)
 
 if __name__ == "__main__":
     reader()
